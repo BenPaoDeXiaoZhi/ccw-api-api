86746a84fa7ea47d2f7c6fc6575b2f9aab04e764
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2026-07-20 18:24:32 +0800

    v1.0.2: enable iife

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,38 @@
 import { defineConfig } from "tsdown";
+
+const entries = {
+  index: "src/index.ts",
+  bfs: "src/bfs-web/index.ts",
+  communityWeb: "src/community-web/index.ts",
+  communityWebDB: "src/community-web-cloud-database/index.ts",
+  gandiMain: "src/gandi-main/index.ts",
+  opParent: "src/op-parent-api/index.ts",
+  sso: "src/sso/index.ts",
+};
+
 export default defineConfig({
-  entry: {
-    index: "src/index.ts",
-    bfs: "src/bfs-web/index.ts",
-    communityWeb: "src/community-web/index.ts",
-    communityWebDB: "src/community-web-cloud-database/index.ts",
-    gandiMain: "src/gandi-main/index.ts",
-    opParent: "src/op-parent-api/index.ts",
-    sso: "src/sso/index.ts",
+  entry: entries,
+  format: {
+    cjs: {
+      outputOptions: {
+        exports: "named",
+      },
+    },
+    esm: {},
+    iife: {
+      entry: { index: "src/index.ts" },
+      deps: {
+        alwaysBundle() {
+          return true;
+        },
+        skipNodeModulesBundle: false,
+        dts: {},
+      },
+      outputOptions: {
+        exports: "named",
+      },
+    },
   },
-  format: ["cjs", "esm"],
   outDir: "dist",
   platform: "browser",
   globalName: "ccwApi",
