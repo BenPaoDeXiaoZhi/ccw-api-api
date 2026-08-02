1374c3e5d349cf189498c1d4e1474683a9268210
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2026-08-02 14:56:28 +0800

    fix test

--- src/community-web/students/update.test.ts
+++ src/community-web/students/update.test.ts
@@ -2,9 +2,5 @@ import { updateStudent } from "./update";
 import { test, testAuthWriteApi } from "src/testUtils";
 
 test("update student", async () => {
-  await testAuthWriteApi(
-    () =>
-      updateStudent("MALE", false, "测试用户", Date.now(), "", "", "", "", ""),
-    {},
-  );
+  await testAuthWriteApi(() => updateStudent({}), {});
 });
